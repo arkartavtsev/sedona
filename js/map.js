2b01efcea1135d6d19ae5a8cf230fa289e26e26f
Artem Kartavtsev <arkartavtsev@live.ru>
2018-07-28 17:49:24 +0700

    Update 2018-07-28T10:48:30.447Z

--- js/map.js
+++ js/map.js
@@ -6,25 +6,40 @@
 
   window.initMap = function () {
     if (mapContainer) {
-      mapContainer.classList.add('sedona-location--js');
-      mapContainer.querySelector('.sedona-location__img').classList.add('sedona-location__img--js');
-      var sedona = {lat: 34.869867, lng: -111.760635};
-      var sedonaMap = new google.maps.Map(document.getElementById('map'), {
-        center: sedona,
-        zoom: 7,
+      var CENTER_COORDS = {
+        lat: 34.869867,
+        lng: -111.760635
+      };
+
+
+      var map = mapContainer.querySelector('#map');
+      var mapProperties = {
+        center: CENTER_COORDS,
+        zoom: 8,
         minZoom: 4,
         disableDoubleClickZoom: true,
         disableDefaultUI: true,
         backgroundColor: '#fff'
-      });
+      };
 
-      var marker = new google.maps.Marker({
-        position: sedona,
+      var sedonaMap = new google.maps.Map(map, mapProperties);
+
+      var markerIcon = {
+        url: 'img/icon-map-marker.svg',
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      var markerProperties = {
+        position: CENTER_COORDS,
         map: sedonaMap,
-        icon: 'img/icon-map-marker.svg'
-      });
-    }
-  }
+        optimized: false,
+        icon: markerIcon
+      };
 
-  console.log('dd');
+      var marker = new google.maps.Marker(markerProperties);
+
+
+      mapContainer.classList.add('sedona-location--js');
+      mapContainer.querySelector('.sedona-location__img').classList.add('sedona-location__img--js');
+    }
+  };
 })();
